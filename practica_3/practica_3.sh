cf05eaeedb7aa07fdacef75e383fb12d3ac5ca45
andresys <andreees.ys@gmail.com>
2023-03-19 18:02:38 +0100

    practica3

--- practica_3/practica_3.sh
+++ practica_3/practica_3.sh
@@ -1,52 +1,86 @@
 #!/bin/bash
-#839756, Vlasceanu, Andrei Gabriel, [M], [3], [A]
-#842236, Yubero Segura, Andres, [M], [3], [A]
-
-# Comprobamos que el usuario tiene privilegios de administración
-if test "$UID" -ne 0
+# Realizar un script que permita añadir y suprimir un conjunto de usuarios especificados en un fichero. Cada línea contendrá 3 
+# campos separados por comas: "<username>,<passwd>,<full_username>"
+# El script debe cumplir los siguientes requisitos:
+#   a) Comprobación de usuario con privilegios de administración. Si el script no está siendo ejecutado por un usuario privilegiado, 
+#      terminará escribiendo por pantalla: "Este script necesita privilegios de administracion" y el status de salida será 1.
+#   b) Dentro de la máquina virtual, el usuario as podrá emplear sudo sin password.
+#   c) La creación o borrado de usuarios se especifica mediante el parámetro [-a|-s]. Cualquier otra opción generará el mensaje de 
+#      error: "Opcion invalida" por la salida de error, stderr.
+#   d) La sintaxis del script es practica_3.sh [-a|-s] <nombre_fichero>. Cuando el número de argumentos sea distinto de 2. El script 
+#      mostrará el siguiente mensaje de error: "Numero incorrecto de parametros"
+#   e) Para borrar usuarios, sólo es necesario el primer campo del fichero. Es decir, se permiten ficheros que sólo tengan un campo.
+#   f) Al añadir usuarios, la caducidad de la nueva contraseña establecida será de 30 días y se escribirá por pantalla el siguiente 
+#      mensaje: "<nombre completo del usuario> ha sido creado".
+#   g) Al añadir usuarios, si el usuario existe, ni se añadirá ni se cambiará su contraseña y se mostrará el siguiente mensaje por 
+#      pantalla: "El usuario <identificador> ya existe". Después de mostrar el mensaje, el script continuará procesando el fichero 
+#      de entrada.
+#   h) Al añadir usuarios, se comprobará que los 3 campos son distintos de la cadena vacía, si alguno de ellos es igual se abortará la 
+#   ejecución y se mostrará el siguiente mensaje: "Campo invalido".
+#   i) Al borrar usuarios, si el usuario no existe, se continuará procesando el fichero sin escribir nada por pantalla.
+#   j) Es necesario utilizar los comandos: useradd, userdel, usermod y chpasswd.
+#   k) Los usuarios deberán tener un UID mayor o igual que 1815.
+#   l) Cada usuario tendrá como grupo por defecto uno con su mismo nombre.
+#   m) El directorio home de cada usuario se inicializará con los ficheros de /etc/skel.
+#   n) El proceso ha de ser completamente automático, sin interacción del administrador.
+#   o) El borrado de usuarios será completo, incluyendo su directorio home.
+#   p) Antes de borrar un usuario, el script realizará un backup de su directorio home (mediante tar y con nombre <usuario>.tar) que 
+#      será guardado en el directorio /extra/backup.
+#   q) Si el argumento es “-s”, borrado usuarios, se debe crear el directorio /extra/backup aunque no se borre ningún usuario.
+#   r) En caso de que el backup no pueda ser completado satisfactoriamente, no se realizará el borrado.
+#
+#"$USER ALL=(ALL) NOPASSWD:ALL"
+if [ $EUID -ne 0 ]; then echo "Este script necesita privilegios de administracion"; exit 1; fi
+if [ $# -eq 2 ]
 then
-    # No tiene privilegios de administración
-    echo "Este script necesita permisos de administracion"
-    exit 1
-fi
-
-# Comprobamos que el fichero tiene exactamente dos parámetros
-if test $# -eq 2
-then 
-    # Comprobamos si tenemos que añadir un usuario
-    if test $1 = "-a"
-    then 
-        echo "Add"
-    # Comprobamos si tenemos que suprimir un usuario
-    elif test $1 = "-s"
+    # ADD USER
+    if [ $1 = "-a" ]
     then
-        # Se crea el directorio de respaldo para los directorios home de los usuarios borrados
-        if test ! -d /extra/backup
-        then
-            mkdir -p /extra/backup
-        fi
-
-        # Leemos cada una de las líneas del fichero (hay una línea por usuario)
-        while read -r linea
+        # READING USER PER USER
+        while IFS= read -r user
         do
-            # Conseguimos el nombre del usuario
-            usuario=$(echo "$linea" | cut -d, -f1)
-            # Obtenemos el directorio home
-            home_dir=$(getent passwd "$usuario" | cut -d: -f6)
-            # Hacer backup del directorio home del usuario
-            tar -cf /extra/backup/$usuario $home_dir &>/dev/null
-            # Si no ha habido ningún problema con el comando anterior
-            if test $? -eq 0
+            IFS=,
+            read -ra user_fields <<< "$user"
+            if [ ${#user_fields[@]} -ne 3 ]; then exit 1; fi
+            for i in "${user_fields[@]}"
+            do 
+                if [ -z i ]; then echo "Campo invalido"; exit 1; fi
+            done
+            # ADDING NEW USER
+            useradd -m -k /etc/skel -U -K UID_MIN=1815 -c "${user_fields[2]}" "${user_fields[0]}" &>/dev/null
+            if [ $? -eq 0 ]
             then
-                # Eliminamos el usuario
-                userdel -fr $usuario &>/dev/null
+                usermod -aG 'sudo' ${user_fields[0]}
+                passwd -x 30 ${user_fields[0]} &>/dev/null
+                echo "${user_fields[0]}:${user_fields[1]}" | chpasswd
+                echo "${user_fields[2]} ha sido creado"
+            else echo "El usuario ${user_fields[0]} ya existe".
             fi
         done < $2
-    # El primer parámetro no es ninguna de las dos opciones anteriores
-    else
-        echo "Opcion invalida">&2
+    # DELETE USER
+    elif [ $1 = "-s" ]
+    then
+        # BACKUP DIRECTORY CREATION
+        if [ ! -d /extra ]; then mkdir -p /extra/backup
+        elif [ ! -d /extra/backup ]; then mkdir /extra/backup
+        fi
+        # READING USER PER USER
+        while IFS= read -r user
+            do
+                IFS=,
+                read -ra user_fields <<< "$user"
+                if [ ${#user_fields[@]} -ne 1 -a ${#user_fields[@]} -ne 3 ]; then exit 1; fi
+                for i in "${user_fields[@]}"
+                do 
+                    if [ -z i ]; then echo "Campo invalido"; exit 1; fi
+                done
+                # ADDING NEW USER
+                user_home="$(getent passwd ${user_fields[0]} | cut -d: -f6)"
+                tar cvf /extra/backup/${user_fields[0]}.tar $user_home &>/dev/null
+                if [ $? -eq 0 ]; then userdel -f ${user_fields[0]} &>/dev/null; fi
+            done < $2
+    # INVALID OPTION
+    else echo "Opcion invalida" 1>&2
     fi
-else
-    # Si no tiene dos parámetros, mostramos el siguiente mensaje por salida estándar
-    echo "Numero incorrecto de parametros"
+else echo "Numero incorrecto de parametros"
 fi
---
+++ practica_3/practica_3_nuestro.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+#839756, Vlasceanu, Andrei Gabriel, [M], [3], [A]
+#842236, Yubero Segura, Andres, [M], [3], [A]
+
+# Comprobamos que el usuario tiene privilegios de administración
+if test "$UID" -ne 0
+then
+    # No tiene privilegios de administración
+    echo "Este script necesita permisos de administracion"
+    exit 1
+fi
+
+# Comprobamos que el fichero tiene exactamente dos parámetros
+if test $# -eq 2
+then 
+    # Comprobamos si tenemos que añadir un usuario
+    if test $1 = "-a"
+    then 
+        # Cambiamos el IFS, guardando su valor actual el oldIFS
+        oldIFS=$IFS
+        IFS=,
+        # Leemos cada una de las líneas del fichero (hay una línea por usuario)
+        while read -r usuario clave nombre
+        do
+            # Comprobamos que no hay campos inválidos
+            if test -z "$usuario" || test -z "$clave" || test -z "$nombre"
+            then
+                echo "Campo invalido"
+            else
+                # Comprobamos si el usuario ya existe
+                if id -u "$usuario" >/dev/null 2>&1
+                then
+                    echo "El usuario $usuario ya existe"
+                else
+                    # Creamos el usuario
+                    useradd -m -k /etc/skel -c "$nombre" -K UID_MIN=1815 -U -p "$clave" "$usuario" &>/dev/null
+                    if test $? -eq 0
+                    then
+                        changepw -R "$usuario"
+                        # Establecemos la contraseña para 30 días
+                        passwd -x 30 "$usuario" &>/dev/null
+                        usermod -aG 'sudo' "$usuario" &>/dev/null
+                        echo "$nombre ha sido creado"
+                    fi
+                fi
+            fi
+        done < $2
+        # Devolvemos el valor original a IFS
+        IFS=$oldIFS
+        
+    # Comprobamos si tenemos que suprimir un usuario
+    elif test $1 = "-s"
+    then
+        # Se crea el directorio de respaldo para los directorios home de los usuarios borrados
+        mkdir -p /extra/backup
+
+        # Leemos cada una de las líneas del fichero (hay una línea por usuario)
+        while read -r linea
+        do
+            # Conseguimos el nombre del usuario
+            usuario=$(echo "$linea" | cut -d, -f1)
+            # Obtenemos el directorio home
+            home_dir=$(getent passwd "$usuario" | cut -d: -f6)
+            # Hacer backup del directorio home del usuario
+            tar -cf "/extra/backup/$usuario.tar" "$home_dir" &>/dev/null
+            # Si no ha habido ningún problema con el comando anterior
+            if test $? -eq 0
+            then
+                # Eliminamos el usuario
+                userdel -fr "$usuario" &>/dev/null
+            fi
+        done < $2
+    # El primer parámetro no es ninguna de las dos opciones anteriores
+    else
+        echo "Opcion invalida">&2
+    fi
+else
+    # Si no tiene dos parámetros, mostramos el siguiente mensaje por salida estándar
+    echo "Numero incorrecto de parametros"
+fi
