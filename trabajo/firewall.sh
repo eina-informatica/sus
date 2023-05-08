c7e9bc36eaf0edb80063da83ae9cf103de3c4aa2
4ndr3s <4ndr3s@duck.com>
2023-05-08 12:28:56 +0200

    trabajo

--- trabajo/firewall.sh
+++ trabajo/firewall.sh
@@ -20,22 +20,25 @@ iptables -t nat -A POSTROUTING -o enp0s8 -j SNAT --to 192.168.57.2
 # Redirección de peticiones desde el NAT al servidor web de Apache de debian2 y al servidor ssh de debian5
 iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 22 -j DNAT --to 192.168.13.2:22
 iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 80 -j DNAT --to 192.168.11.2:80
+iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 443 -j DNAT --to 192.168.11.2:443
 
 # Redirección de peticiones desde el host al servidor web de Apache de debian2 y al servidor ssh de debian5
 iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 22 -j DNAT --to 192.168.13.2:22
 iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j DNAT --to 192.168.11.2:80
+iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 443 -j DNAT --to 192.168.11.2:443
 
-# Permitimos el paso del tráfico hacia la extranet (SSH y servidor web)
+# Se permite el paso del tráfico hacia la extranet (SSH y servidor web)
 iptables -A FORWARD -i enp0s3 -p all -j ACCEPT
 iptables -A FORWARD -i enp0s8 -p all -j ACCEPT
 
-# Permitimos todo el paso hacia red interna 1 y red interna 2
+# Se permite todo el paso hacia red interna 1 y red interna 2
 iptables -A FORWARD -i enp0s9 -p all -j ACCEPT
 iptables -A FORWARD -i enp0s10 -p all -j ACCEPT
 
-# Se permite el tráfico hacia debian5 por el puerto 22 (ssh) y hacia debian2 por el puerto 80(http)
+# Se permite el tráfico hacia debian5 por el puerto 22 (ssh) y hacia debian2 por el puerto 80 y 443 (http)
 iptables -A FORWARD -d 192.168.13.2 -p tcp --dport 22 -j ACCEPT
 iptables -A FORWARD -d 192.168.11.2 -p tcp --dport 80 -j ACCEPT
+iptables -A FORWARD -d 192.168.11.2 -p tcp --dport 443 -j ACCEPT
 
 # Permite que entre todo el tráfico de intranet y la respuesta del host a los pings
 #iptables -A INPUT -i enp0s8 -p icmp --icmp-type 0 -j ACCEPT
@@ -45,6 +48,12 @@ iptables -A INPUT -i enp0s9 -p all -j ACCEPT
 iptables -A INPUT -i enp0s10 -p all -j ACCEPT
 #iptables -A INPUT -i enp0s3 -m state --state ESTABLISHED,RELATED -j ACCEPT
 #iptables -A INPUT -i enp0s8 -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+# Se hace logging
+iptables -A INPUT -i enp0s3 -j LOG
+iptables -A INPUT -i enp0s8 -j LOG
+iptables -A FORWARD -i enp0s3 -j LOG
+iptables -A FORWARD -i enp0s8 -j LOG
 
 # Preservación de las reglas iptables
 iptables-save > /etc/iptables/rules.v4
