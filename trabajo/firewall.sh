ef1978a995f8944bc51a3d4601a8ed57ecfa28cd
4ndr3s <4ndr3s@duck.com>
2023-05-17 14:09:01 +0200

    trabajo

--- trabajo/firewall.sh
+++ trabajo/firewall.sh
@@ -21,13 +21,13 @@ iptables -t nat -A POSTROUTING -o enp0s8 -j SNAT --to 192.168.57.2
 #iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 22 -j DNAT --to 192.168.13.2:22
 #iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 80 -j DNAT --to 192.168.11.2:80
 #iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 443 -j DNAT --to 192.168.11.2:443
-iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 80 -j DNAT --to-destination 192.168.11.2:80
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 192.168.11.2:80
 
 # Redirección de peticiones desde el host al servidor web de Apache de debian2 y al servidor ssh de debian5
 #iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 22 -j DNAT --to 192.168.13.2:22
 #iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j DNAT --to 192.168.11.2:80
 #iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 443 -j DNAT --to 192.168.11.2:443
-iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j DNAT --to-destination 192.168.11.2:80
+#iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j DNAT --to-destination 192.168.11.2:80
 
 # Se permite el paso del tráfico hacia la extranet (SSH y servidor web)
 iptables -A FORWARD -i enp0s3 -p all -j ACCEPT
@@ -42,8 +42,8 @@ iptables -A FORWARD -i enp0s10 -p all -j ACCEPT
 #iptables -A FORWARD -d 192.168.11.2 -p tcp --dport 80 -j ACCEPT
 #iptables -A FORWARD -d 192.168.11.2 -p tcp --dport 443 -j ACCEPT
 iptables -A FORWARD -p tcp --dport 80 -d 192.168.11.2 -j ACCEPT
-iptables -A FORWARD -p tcp --sport 80 -s 192.168.11.2 -j ACCEPT
-iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+#iptables -A FORWARD -p tcp --sport 80 -s 192.168.11.2 -j ACCEPT
+#iptables -A FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 
 # Permite que entre todo el tráfico de intranet y la respuesta a conexiones ya establecidas
 #iptables -A INPUT -i enp0s8 -p icmp --icmp-type 0 -j ACCEPT
@@ -53,6 +53,8 @@ iptables -A INPUT -i enp0s9 -p all -j ACCEPT
 iptables -A INPUT -i enp0s10 -p all -j ACCEPT
 #iptables -A INPUT -i enp0s3 -m state --state ESTABLISHED,RELATED -j ACCEPT
 #iptables -A INPUT -i enp0s8 -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+iptables -t nat -A POSTROUTING -j MASQUERADE
 
 # Se hace logging
 iptables -A INPUT -i enp0s3 -j LOG
