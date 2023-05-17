9fe5ef8017fc9b0ddd56605d7c9036c1839d5133
4ndr3s <4ndr3s@duck.com>
2023-05-17 13:17:50 +0200

    practica5

--- trabajo/firewall.sh
+++ trabajo/firewall.sh
@@ -18,14 +18,15 @@ iptables -t nat -A PREROUTING -i enp0s8 -j DNAT --to 192.168.57.2
 iptables -t nat -A POSTROUTING -o enp0s8 -j SNAT --to 192.168.57.2
 
 # Redirección de peticiones desde el NAT al servidor web de Apache de debian2 y al servidor ssh de debian5
-iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 22 -j DNAT --to 192.168.13.2:22
-iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 80 -j DNAT --to 192.168.11.2:80
-iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 443 -j DNAT --to 192.168.11.2:443
+#iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 22 -j DNAT --to 192.168.13.2:22
+#iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 80 -j DNAT --to 192.168.11.2:80
+#iptables -t nat -A PREROUTING -i enp0s3 -p tcp --dport 443 -j DNAT --to 192.168.11.2:443
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j DNAT --to-destination 192.168.11.2:80
 
 # Redirección de peticiones desde el host al servidor web de Apache de debian2 y al servidor ssh de debian5
-iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 22 -j DNAT --to 192.168.13.2:22
-iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j DNAT --to 192.168.11.2:80
-iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 443 -j DNAT --to 192.168.11.2:443
+#iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 22 -j DNAT --to 192.168.13.2:22
+#iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 80 -j DNAT --to 192.168.11.2:80
+#iptables -t nat -A PREROUTING -i enp0s8 -p tcp --dport 443 -j DNAT --to 192.168.11.2:443
 
 # Se permite el paso del tráfico hacia la extranet (SSH y servidor web)
 iptables -A FORWARD -i enp0s3 -p all -j ACCEPT
@@ -36,9 +37,11 @@ iptables -A FORWARD -i enp0s9 -p all -j ACCEPT
 iptables -A FORWARD -i enp0s10 -p all -j ACCEPT
 
 # Se permite el tráfico hacia debian5 por el puerto 22 (ssh) y hacia debian2 por el puerto 80 y 443 (http)
-iptables -A FORWARD -d 192.168.13.2 -p tcp --dport 22 -j ACCEPT
-iptables -A FORWARD -d 192.168.11.2 -p tcp --dport 80 -j ACCEPT
-iptables -A FORWARD -d 192.168.11.2 -p tcp --dport 443 -j ACCEPT
+#iptables -A FORWARD -d 192.168.13.2 -p tcp --dport 22 -j ACCEPT
+#iptables -A FORWARD -d 192.168.11.2 -p tcp --dport 80 -j ACCEPT
+#iptables -A FORWARD -d 192.168.11.2 -p tcp --dport 443 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 80 -d 192.168.11.2 -j ACCEPT
+#iptables -A FORWARD -p tcp --sport 80 -s 192.168.11.2 -j ACCEPT
 
 # Permite que entre todo el tráfico de intranet y la respuesta a conexiones ya establecidas
 iptables -A INPUT -i enp0s8 -p icmp --icmp-type 0 -j ACCEPT
